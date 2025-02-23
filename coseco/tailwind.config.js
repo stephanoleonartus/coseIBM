197c3694cc27983a58445c5746298f8a056f6c13
steph-leo <leostephs89@gmail.com>
2025-02-24 02:45:17 +0300

    add file

---
+++ coseco/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // Adjust this to match your file structure
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
